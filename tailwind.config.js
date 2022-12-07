6933c72e8eba3eee282e75f24f309107712aded4
Muhammad Faris Rafi <farisrafi048@gmail.com>
2022-12-08 03:12:16 +0700

    restorefiles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.js',
+    'node_modules/flowbite-react/**/*.js'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+}
